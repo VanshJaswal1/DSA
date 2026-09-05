724fd29f7c98ac6d24d5635312fd9d23205acc8d
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-09-05 13:15:23 +0530

    add the new one s

--- insertionatany2.cpp
+++ insertionatany2.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
-    
+     int arr[10]= { 1,2,3,4,5};
+     int index = 1;
+     int n = 5;
+     int value = 6;
+     for(int i = 5 ; i<index ; i--){
+        arr[i] = arr[i-1];
+     }
+      arr[index] = value;
+      n++;
+      for(int j =0 ; j<=5 ; j++) {
+        cout<< arr[j];
+          }
+
     return 0;
 }
