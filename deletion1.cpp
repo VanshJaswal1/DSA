29bd4611bbefa97bd513062f484c72283e576626
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-09-05 12:41:43 +0530

    add the new one s

--- deletion1.cpp
+++ deletion1.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main()
 {
-    
+     int arr[] = {3,4,5,6,7};
+     int Arr[5];
+     for( int i = 1 ; i<=4; i++) {
+        Arr[i] = arr[i];
+     }
+     for(int j = 1 ; j<=4; j++) {
+        cout << Arr[j];
+     }
     return 0;
 }
--- deletion2.cpp
+++ deletion2.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main()
 {
-    
+     int arr[] = {3,4,5,6,7};
+     int Arr[5];
+     for( int i = 0 ; i<=4; i++) {
+        Arr[i] = arr[i];
+     }
+     for(int j = 0 ; j<=3; j++) {
+        cout << Arr[j];
+     }
     return 0;
 }
