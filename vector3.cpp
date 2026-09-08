7be90c10d92f4d409514ee6c64fa57cbe292089d
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-09-08 23:44:00 +0530

    addddd them

--- vector3.cpp
+++ vector3.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    
+      vector<int>arr = {1,2,3,4,5};
+     arr.erase(arr.begin() + 2  );
+     for(int i = 0 ; i< arr.size() ; i++){
+        cout<<arr[i];
+     }
     return 0;
 }
