56fad6d26b44f6dcdd8a9ff391a0b21e55526fce
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-09-08 23:31:22 +0530

    adddd them

--- vector1.cpp
+++ vector1.cpp
@@ -1,10 +1,17 @@
-#include<iostream>
-#include <vector>
-using namespace std;
+    #include<iostream>
+    #include <vector>
+    using namespace std;
 
-int main()
-{
-    vector<int>arr;
-    
-    return 0;
-}
+    int main()
+    {
+        vector<int>arr = {10,20,30,40};
+        cout<<arr[1];
+        arr.push_back(50);
+        //   cout<<arr[4];
+        arr.pop_back();
+        for(int i = 0 ; i<arr.size(); i++){
+            cout<<arr[i];
+        }
+
+        return 0;
+    }
