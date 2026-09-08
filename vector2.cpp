7df74acbbb1bf2ae1c9a69cf932e503790144200
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-09-08 23:36:31 +0530

    adddd them

--- vector2.cpp
+++ vector2.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main()
-{
+{    vector<int> arr = {1,2,3,4,5,6};
+     arr.insert(arr.begin()+ 2 , 7);
+     for(int i = 0; i< arr.size(); i++){
+        cout<<arr[i];
+     }
+
     
     return 0;
 }
